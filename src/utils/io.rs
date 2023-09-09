011d57787a1419089a7e9d0c2652fba887b1e8b2
Huterok228 <clash.of.clans23916@gmail.com>
2023-09-09 18:06:12 +0300

    minor changes

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,7 +1,9 @@
 use anyhow::{bail, Ok, Result};
 
 pub fn read_password() -> Result<String> {
-    let password: String = rpassword::prompt_password("Your password: ")?;
+    let password: String = rpassword::prompt_password("Your password: ")?
+        .trim()
+        .to_owned();
     if password.len() < 4 {
         bail!("Password is too short")
     }
